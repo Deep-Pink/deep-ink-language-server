b66aa903766db58a525c813ad4103e7ca14071d3
Natalie Cuthbert <natalie@cuthbert.co.za>
2026-08-05 15:16:23 +0200

    Added type-sitter to project

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod deep_ink_nodes {
+    include!(concat!(env!("OUT_DIR"), "/deep_ink_nodes.rs"));
+}
+
+// mod deep_ink_queries {
+//     include!(concat!(env!("OUT_DIR"), "/deep_ink_queries.rs"));
+// }
+
+mod ink_nodes {
+    include!(concat!(env!("OUT_DIR"), "/ink_nodes.rs"));
+}
+
+// mod ink_queries {
+//     include!(concat!(env!("OUT_DIR"), "/ink_queries.rs"));
+// }
